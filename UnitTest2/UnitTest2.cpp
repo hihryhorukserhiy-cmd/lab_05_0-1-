b459725dbf9e22b69712a32b0062c347a19a0242
hihryhorukserhiy-cmd <hihryhorukserhiy@gmail.com>
2025-11-07 15:45:28 +0200

    test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_05_1/lab_05_1.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double a = 1.0;
+			double b = 0.0;
+
+
+			double expected = 1.0; // значення яке б мала надати функція h для цих вхідних даних	
+
+			double actual = h(a, b);
+
+
+			double tolerance = 0.0000001; // допустима похибка	
+			Assert::AreEqual(expected, actual, tolerance);
+		}
+	};
+}
