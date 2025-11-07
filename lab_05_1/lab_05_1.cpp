79e6c570b51590b7e4425bdda1119b652eddc2c8
hihryhorukserhiy-cmd <hihryhorukserhiy@gmail.com>
2025-11-07 15:41:49 +0200

    first

---
+++ lab_05_1/lab_05_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+#include <cmath> 
+using namespace std;
+double h(const double a, const double b);
+int main()
+{
+    double s, t;
+    cout << "s = "; cin >> s;
+    cout << "t = "; cin >> t;
+
+    double u = (h(1, s * t) + pow(h(t + s, 1), 2)) / (1 + pow(h(s, t), 3));
+
+    cout << "u = " << u << endl;
+
+    return 0;
+}
+
+double h(const double a, const double b)
+{
+    return (a * a - sin(b) * cos(a) + b * b);
+}
